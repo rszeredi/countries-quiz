d8c9def182aaf51fdda172204a7cc5b3380d04e9
Ria Szeredi <ria.szeredi@gmail.com>
2022-03-20 12:58:06 -0700

    add progress bars to scoreboxes

--- src/ScoreBox.js
+++ src/ScoreBox.js
@@ -3,12 +3,20 @@ import './ScoreBox.css';
 
 class ScoreBox extends Component {
 	render() {
-		const { scoreValue, scoreName } = this.props;
+		const { scoreValue, scoreName, color, width } = this.props;
 		return (
 			<div className="ScoreBox">
 				<div>{scoreValue}</div>
 				<div>{scoreName}</div>
-				<div className="ScoreBox-score-bar" />
+				<div
+					className="ScoreBox-score-bar"
+					style={{ backgroundColor: color.replace('100%', '30%'), position: 'relative' }}
+				>
+					<div
+						className="ScoreBox-score-bar"
+						style={{ backgroundColor: color, position: 'absolute', width: width }}
+					/>
+				</div>
 			</div>
 		);
 	}
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -6,11 +6,27 @@ import './ScoreCard.css';
 class ScoreCard extends Component {
 	render() {
 		const { remaining, correct, incorrect } = this.props;
+		const total = correct + incorrect + remaining;
 		return (
 			<div className="ScoreCard">
-				<ScoreBox scoreValue={remaining} scoreName="Remaining" />
-				<ScoreBox scoreValue={incorrect} scoreName="Incorrect" />
-				<ScoreBox scoreValue={correct} scoreName="Correct" />
+				<ScoreBox
+					scoreValue={remaining}
+					scoreName="Remaining"
+					color="rgba(67, 67, 242, 100%)"
+					width={`${100 * remaining / total}%`}
+				/>
+				<ScoreBox
+					scoreValue={incorrect}
+					scoreName="Incorrect"
+					color="rgba(232, 70, 70, 100%)"
+					width={`${100 * incorrect / total}%`}
+				/>
+				<ScoreBox
+					scoreValue={correct}
+					scoreName="Correct"
+					color="rgba(9, 213, 67, 100%)"
+					width={`${100 * correct / total}%`}
+				/>
 			</div>
 		);
 	}
